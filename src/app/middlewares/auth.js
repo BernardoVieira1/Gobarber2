69f52c8ceb8021fb3362f803028700e4fc9b90ad
BernardoVieira1 <bernardovieira50@gmail.com>
2022-04-20 00:31:44 -0300

    middleware de autenticacao

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,27 @@
+import jwt from 'bcryptjs';
+import { promisify } from 'util';
+
+
+import authConfig from '../../config/auth';
+
+export default async(req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return req.status(401).json({ error: 'token not provided' })
+  }
+
+  const [bearer, token] = authHeader.split(' ');
+
+  try{
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req,userId = decoded.id;
+
+    return next();
+
+  }catch(err){
+    return res.status(401).json({ error: 'Token invalid' })
+  }
+
+};
